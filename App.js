012b4d84d712f13c2655f5d27d4d8f8a93ee5910
netLee <wltjr1426@naver.com>
2021-02-24 11:26:03 +0900

    Create Loading screen

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component {
+  getLocation = async () => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "skyblue",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "white",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     // flexDirction: "row" 가로로 정렬 -> default는 column
+//     // alignItems: "center",
+//     // justifyContent: "center",
+//     flex: 1, // 부모 클래스, 모든 공간이 사용 가능하도록
+//   },
+//   yellowView: {
+//     flex: 1, // 숫자가 높을수록 더 많은 공간 차지
+//     backgroundColor: "green",
+//   },
+//   blueView: {
+//     flex: 1,
+//     backgroundColor: "blue",
+//   },
+// });
