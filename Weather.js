e8588c8aea0fc0baaf7ac30b8649a81a8d2e1a9c
netLee <wltjr1426@naver.com>
2021-03-01 12:52:43 +0900

    Adjust code

--- Weather.js
+++ Weather.js
@@ -5,23 +5,61 @@ import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"],
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"],
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", "#FEF253"],
+  },
   Haze: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+  },
+  Mist: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
   },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+  },
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Haze",
+    subtitle: "Just don't go outside.",
+  },
 };
 
-export default function Weather({ temp }) {
+export default function Weather({ temp, condition }) {
   return (
     <LinearGradient
-      colors={weatherOptions["Haze"].gradient}
+      colors={weatherOptions["Clear"].gradient}
       style={styles.container}
     >
       <StatusBar barStyle={"light-content"} />
       <View style={styles.halfContainer}>
         <MaterialCommunityIcons
           size={92}
-          name={weatherOptions["Haze"].iconName}
+          name={weatherOptions[condition].iconName}
           color="white"
         />
         <Text style={styles.temp}>{temp}℃</Text>
